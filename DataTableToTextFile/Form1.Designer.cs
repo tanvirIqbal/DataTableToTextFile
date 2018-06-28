88529031b46661345ca0af9c06b950905c678459
tanviriqbal <tanviriqbal@CEL52>
2018-06-28 10:40:05 +0600

    1st Commit

---
+++ DataTableToTextFile/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace DataTableToTextFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSetPath = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCreateFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSetPath
+            // 
+            this.btnSetPath.Location = new System.Drawing.Point(482, 22);
+            this.btnSetPath.Name = "btnSetPath";
+            this.btnSetPath.Size = new System.Drawing.Size(70, 22);
+            this.btnSetPath.TabIndex = 15;
+            this.btnSetPath.Text = "Set Path";
+            this.btnSetPath.UseVisualStyleBackColor = true;
+            this.btnSetPath.Click += new System.EventHandler(this.btnSetPath_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(157, 23);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(319, 20);
+            this.txtPath.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "File Path";
+            // 
+            // btnCreateFile
+            // 
+            this.btnCreateFile.Location = new System.Drawing.Point(477, 68);
+            this.btnCreateFile.Name = "btnCreateFile";
+            this.btnCreateFile.Size = new System.Drawing.Size(75, 25);
+            this.btnCreateFile.TabIndex = 16;
+            this.btnCreateFile.Text = "Create File";
+            this.btnCreateFile.UseVisualStyleBackColor = true;
+            this.btnCreateFile.Click += new System.EventHandler(this.btnCreateFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 105);
+            this.Controls.Add(this.btnCreateFile);
+            this.Controls.Add(this.btnSetPath);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Datatable to Text";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSetPath;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCreateFile;
+    }
+}
+
---
+++ DataTableToTextFile/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataTableToTextFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string _sFolderPath = string.Empty;
+        private void btnSetPath_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog oDialog = new FolderBrowserDialog();
+                if (oDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtPath.Text = oDialog.SelectedPath + "\\";
+                }
+                else
+                {
+                    return;
+                }
+
+                if (txtPath.Text.Trim().Length > 0)
+                    _sFolderPath = txtPath.Text;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Path cannot be set due to " + ex.Message, "Path Selection", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCreateFile_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Phone", typeof(string));
+            DataRow dRow = dt.NewRow();
+            dRow["Name"] = "Name 1";
+            dRow["Phone"] = "Phone 1";
+            dt.Rows.Add(dRow);
+            dRow = dt.NewRow();
+            dRow["Name"] = "Name 2";
+            dRow["Phone"] = "Phone 2";
+            dt.Rows.Add(dRow);
+            dRow = dt.NewRow();
+            dRow["Name"] = "Name 3";
+            dRow["Phone"] = "Phone 3";
+            dt.Rows.Add(dRow);
+
+            string sStringToWrite = string.Empty;
+            string sSuccess = string.Empty;
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                sStringToWrite += dr["Name"].ToString().Trim() + "\t\t" +
+                                    dr["Phone"].ToString().Trim();
+                sStringToWrite += Environment.NewLine;
+            }
+
+            if (sStringToWrite.Trim().Length > 0)
+            {
+                sSuccess += WriteToTxtFile(sStringToWrite, _sFolderPath, "Contact List") + Environment.NewLine;
+                sStringToWrite = string.Empty;
+            }
+            if (sSuccess.Trim().Length > 0)
+                MessageBox.Show(sSuccess);
+        }
+
+        public static string WriteToTxtFile(string sStringToWrite, string sFilePath, string sFileName)
+        {
+            string sSuccess = string.Empty;
+
+            try
+            {
+                DirectoryInfo oFileDirectory = new DirectoryInfo(sFilePath);
+                if (!oFileDirectory.Exists)
+                {
+                    oFileDirectory.Create();
+                }
+
+                StreamWriter oSWriter = new StreamWriter(sFilePath + "/" + sFileName + ".txt", false);
+                oSWriter.WriteLine(sStringToWrite);
+                sSuccess = "You can find a file named - " + sFileName + " at " + sFilePath;
+                oSWriter.Flush();
+                oSWriter.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return sSuccess;
+        }
+    }
+}
